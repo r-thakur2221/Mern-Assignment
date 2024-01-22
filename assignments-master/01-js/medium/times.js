524a5386b8d57178cdae511e393c02647bdaad09
Rahul Thakur <thakur.r2221@gmail.com>
2024-01-21 19:17:37 -0500

    calculated time taken by program to run in 01-js

--- assignments-master/01-js/medium/times.js
+++ assignments-master/01-js/medium/times.js
@@ -8,6 +8,25 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function sum(n){
+    let sum=0;
+    /*
+        loop is taking time longer with longer input
+    */
+    for(let i=1;i<=n;i++){
+        sum+=i;
+    }
+    sum=(n*(n+1))/2;//this is taking 0ms using formula
+    return sum;
 }
+
+function calculateTime(n) {
+    let t1=Date.now();
+    let sumAll=sum(n);
+    let t2=Date.now();
+    return t2-t1;
+}
+
+console.log(calculateTime(100));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000000));
